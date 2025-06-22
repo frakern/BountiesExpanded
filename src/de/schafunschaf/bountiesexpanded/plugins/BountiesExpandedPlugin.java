585e05575b42f2fdaf671440867b48252a3a7766
Star <star@star.com>
2025-06-22 14:52:15 -0700

    Switch settings to use lunalib.

--- src/de/schafunschaf/bountiesexpanded/plugins/BountiesExpandedPlugin.java
+++ src/de/schafunschaf/bountiesexpanded/plugins/BountiesExpandedPlugin.java
@@ -23,6 +23,7 @@ import de.schafunschaf.bountiesexpanded.scripts.campaign.intel.bounties.warcrimi
 import de.schafunschaf.bountiesexpanded.scripts.campaign.intel.bounties.warcriminal.WarCriminalManager;
 import de.schafunschaf.bountiesexpanded.scripts.campaign.intel.entity.BountyEntity;
 import lombok.extern.log4j.Log4j;
+import lunalib.lunaSettings.LunaSettings;
 
 import java.util.Set;
 
@@ -32,7 +33,6 @@ import static de.schafunschaf.bountiesexpanded.util.ComparisonTools.isNull;
 @Log4j
 public class BountiesExpandedPlugin extends BaseModPlugin {
     public static final String DEFAULT_BLACKLIST_FILE = "data/config/bountiesExpanded/default_blacklist.json";
-    public static final String SETTINGS_FILE = "bounties_expanded_settings.ini";
     public static final String NAME_STRINGS_FILE = "data/config/bountiesExpanded/name_strings.json";
     public static final String RARE_FLAGSHIPS_FILE = "data/config/vayraBounties/rare_flagships.csv";
 
@@ -54,7 +54,7 @@ public class BountiesExpandedPlugin extends BaseModPlugin {
 
         reloadHullMods();
 
-        if (Settings.sheepDebug) printDebugInfo();
+        if (Settings.debug) printDebugInfo();
 
         if (newGame)
             spawnInitialBounties();
@@ -162,7 +162,7 @@ public class BountiesExpandedPlugin extends BaseModPlugin {
     }
 
     private void initBountiesExpanded() {
-        loadSettings(SETTINGS_FILE);
+        LunaSettings.addSettingsListener(new Settings());
         loadBlacklists(DEFAULT_BLACKLIST_FILE);
         loadNameStringFiles(NAME_STRINGS_FILE);
     }
